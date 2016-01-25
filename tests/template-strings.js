6dbfc94eec5b3ec280193896afa575ef2ff23b70
andersonmcook <andersonmcook@gmail.com>
2016-01-25 15:21:44 -0600

    new line test and expression evaluation test

--- tests/template-strings.js
+++ tests/template-strings.js
@@ -5,4 +5,29 @@ describe('template strings', function () {
     var string = `abc`;
     expect(string).to.be.a("string");
   });
+
+  it('can evaluate variables', function () {
+    var bar = 'baz';
+    var string = `foo ${bar}`;
+
+    expect(string).to.equal('foo baz');
+  });
+
+  it('can execute functions', function () {
+    var string = `The date is: ${new Date()}`;
+    var date = new Date();
+    expect(string).to.equal("The date is: " + date);
+  });
+
+  it('can print multiline strings', function () {
+    var string = `hey
+yo`;
+    expect(string).to.equal("hey\nyo");
+  });
+
+  it('can evaluate expresions', function () {
+    var string = `${3 + 6}`;
+    expect(string).to.equal("9");
+  });
 });
+
