3a71ad061e0a1fc32db584b9f9507d46326369c3
andersonmcook <andersonmcook@gmail.com>
2016-01-25 19:45:28 -0600

    block scope test

--- tests/block-scope.js
+++ tests/block-scope.js
@@ -2,16 +2,27 @@
 var expect = require('chai').expect;
 
 describe('block scope', function () {
-  it('block scope', function () {
+  it('block scope 1', function () {
     var fn = function () {
       let three = 3;
-      console.log("three outside", three);
+      //console.log("three outside", three);
       if (true) {
         let three = "four";
-        console.log("three inside", three);
+        //console.log("three inside", three);
       }
       return three;
     };
     expect(fn()).to.equal(3);
+  });
+
+  it('block scope 2', function () {
+    var fn = function () {
+      let array = [];
+      for (let i = 0; i < 4; i++) {
+        array.push(i);
+      }
+      return array.reduce((a, b) => a + b);
+    };
+    expect(fn()).to.equal(6);
   });
 });
